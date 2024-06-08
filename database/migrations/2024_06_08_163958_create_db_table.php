ec1a555d4e3c86cb6a5540647c69c4040bcbd649
ejme1992 <edwin.jme@hotmail.com>
2024-06-08 15:11:31 -0300

    feat: Create migrations for user surveys and answers

---
+++ database/migrations/2024_06_08_163958_create_db_table.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('users_answers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('registration_date')->useCurrent();
+            $table->timestamps();
+        });
+
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamp('creation_date')->useCurrent();
+            $table->timestamps();
+        });
+
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('survey_id')->constrained('surveys')->onDelete('cascade');
+            $table->string('text');
+            $table->string('type');
+            $table->timestamps();
+        });
+
+        Schema::create('options', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('question_id')->constrained('questions')->onDelete('cascade');
+            $table->string('text');
+            $table->timestamps();
+        });
+
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users_answers')->onDelete('cascade');
+            $table->foreignId('survey_id')->constrained('surveys')->onDelete('cascade');
+            $table->foreignId('question_id')->constrained('questions')->onDelete('cascade');
+            $table->foreignId('option_id')->nullable()->constrained('options')->onDelete('cascade');
+            $table->text('answer')->nullable();
+            $table->integer('time_spent')->nullable();
+            $table->timestamp('answer_date')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+        Schema::dropIfExists('options');
+        Schema::dropIfExists('questions');
+        Schema::dropIfExists('surveys');
+        Schema::dropIfExists('users_answers');
+    }
+};
