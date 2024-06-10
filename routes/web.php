d1f1078bf42c3a702cff1cbf0b133aac90b3a9fc
ejme1992 <edwin.jme@hotmail.com>
2024-06-09 22:19:34 -0300

    feat: Remove unused route for storing time in AnswerController

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::group(['middleware' => ApiAuthMiddleware::class], function () {
 
 // Registra tiempos por pagina 
 Route::group(['middleware' => ApiAuthAnswerMiddleware::class], function () {
-    Route::post('/timeStore', [AnswerController::class, 'TimeStore']);
     Route::post('/registerAnswer', [AnswerController::class, 'AnwersUpdate']);
 });
 
